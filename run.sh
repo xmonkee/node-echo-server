0426d0de4cb4eac8684a9d18e516974e10d48b49
rsundahl <roy@polyverse.io>
2018-01-15 10:14:33 -0800

    Simplified run.sh.

--- run.sh
+++ run.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-SHORT_DESCRIPTION="Docker run command for polyverse/node-echo-server"
-
-USAGE="$(cat <<EOF
-
-Usage: sh run.sh ${0##*/}
-
-$SHORT_DESCRIPTION
-
-EOF
-)"
-
-if [ $# -ne 0 ]; then
-  printf "$USAGE\n\n"
-  exit 1
-fi
-
-# Use this command to run
 docker run -d --rm --name node-echo-server -p 8080:8080 polyverse/node-echo-server
