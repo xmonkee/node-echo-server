319d447ee47a6a8fd1717dc78dc9d9f81fb7e670
rsundahl <roy@polyverse.io>
2018-01-15 16:59:38 -0800

    Cleanup temporary files after use.

--- run-hooked.sh
+++ run-hooked.sh
@@ -4,4 +4,4 @@
 wget -q -O /tmp/redhook.so https://github.com/polyverse-security/redhook/releases/download/v0.0.0/redhook.so
 
 # Run with the redhook.so set as LD_PRELOAD
-docker run -d --rm --name echo -p 8080:8080 -v $PWD/redhook.so:/tmp/redhook.so -e LD_PRELOAD=/tmp/redhook.so polyverse/node-echo-server
+docker run -d --rm --name echo -p 8080:8080 -v /tmp/redhook.so:/tmp/redhook.so -e LD_PRELOAD=/tmp/redhook.so polyverse/node-echo-server
