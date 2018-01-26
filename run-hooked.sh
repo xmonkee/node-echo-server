16f3f2422fc2cd187383c55b359984d671552fd8
rsundahl <roy@polyverse.io>
2018-01-26 14:26:10 -0800

    Modified run-hooked.sh to allow list of hooks.

--- run-hooked.sh
+++ run-hooked.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+REPOSITORY=polyverse/readhook
+RELEASE_TAG=$1
 
-# Get a copy of readhook.so
-wget -q -O /tmp/readhook.so https://github.com/polyverse/readhook/releases/download/v0.0.0/readhook.so
+if [[ "$RELEASE_TAG" == "" ]]; then
+	RELEASE_TAG=jenkins
+fi
+
+# Get a copy of makeload.so and readhook.so
+wget -q -O /tmp/makeload.so https://github.com/$REPOSITORY/releases/download/$RELEASE_TAG/makeload.so
+wget -q -O /tmp/readhook.so https://github.com/$REPOSITORY/releases/download/$RELEASE_TAG/readhook.so
+
+#cp ../readhook/makeload.so /tmp/makeload.so
+#cp ../readhook/readhook.so /tmp/readhook.so
 
 # Run with the readhook.so set as LD_PRELOAD
-docker run -it --rm --name echo -p 8080:8080 -v /tmp/readhook.so:/tmp/readhook.so -e LD_PRELOAD=/tmp/readhook.so polyverse/node-echo-server
+docker run -it --rm --name echo -p 8080:8080 -v /tmp/makeload.so:/tmp/makeload.so -v /tmp/readhook.so:/tmp/readhook.so -e LD_PRELOAD="/tmp/makeload.so /tmp/readhook.so" polyverse/node-echo-server
