daed7b69ee5a6c17214efb4ba474d58205ace951
rsundahl <roy@polyverse.com>
2020-03-03 13:46:28 -0800

    Use latest build/test/publish pattern

--- build.sh
+++ build.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
+set -e
 
-buildtarget()
-{
-	command="docker build --no-cache -t polyverse/node-echo-server -t internal.hub.polyverse.io/node-echo-server ."
-	echo $command
-	$command
+declare -r PV_DOCKER_REGISTRY="polyverse"
+declare -r PV_NAME="node-echo-server"
+
+main() {
+        build
+        [ $? -ne 0 ] && return 1
+
+        return 0
 }
 
-	buildtarget "$@"
+build() {
+        # Build the image
+        docker build -t "${PV_NAME}" -t "${PV_DOCKER_REGISTRY}/${PV_NAME}:latest" .
+        [ $? -ne 0 ] && return 1
 
-if [ "$1" == "-p" ]; then
-	docker push polyverse/node-echo-server
-	docker push internal.hub.polyverse.io/node-echo-server
-fi
+        # Don't tag when there are uncommitted changes.
+        if [ -z "$(git status -s -uno)" ]; then
+		declare -r PV_GIT_COMMIT="$(git rev-parse --verify HEAD)"
+
+		docker tag "${PV_DOCKER_REGISTRY}/${PV_NAME}:latest" "${PV_DOCKER_REGISTRY}/${PV_NAME}:${PV_GIT_COMMIT}"
+        	[ $? -ne 0 ] && return 1
+        fi
+
+        return 0
+}
+
+main "$@"
+exit $?
